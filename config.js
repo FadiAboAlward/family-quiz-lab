cf3a2d5624d2e4c0dbeee98f7729921fd92da14e
FadiAboAlward <fadi.aljenneyat@gmail.com>
2026-09-01 20:22:00 +0300

    Connect Quiz Lab to isolated Supabase backend

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.QUIZ_LAB_CONFIG = {
-  cloudEnabled: false,
-  supabaseUrl: "",
-  supabasePublishableKey: "",
+  cloudEnabled: true,
+  supabaseUrl: "https://yxjkjqmstcsczfkxytxc.supabase.co",
+  supabasePublishableKey: "sb_publishable_4F9tveZyBkB7WFLiTWQlWw_okTlsEs-",
   saveRpc: "save_quiz_lab_attempt",
   loadRpc: "get_quiz_lab_attempt"
 };
